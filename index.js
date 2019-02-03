1755c1533ac6e9fb77e40881186b53418c3b02bd
zhusijia3 <zhusijia3@jd.com>
2019-02-03 21:42:48 +0800

    git init

---
+++ index.js
@@ -0,0 +1,3 @@
+// index.js
+import toUpper from './upper.js';
+export default toUpper;
---
+++ upper.js
@@ -0,0 +1,5 @@
+// upper.js
+let toUpper = (a) => {
+    return a.toUpperCase();
+};
+export default toUpper;
