c6b5d536f16339cf96bc7edd80314bbebe0e9866
zhusijia3 <zhusijia3@jd.com>
2019-02-04 15:22:57 +0800

    update

---
+++ src/components/Button/Button.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+class Input extends Component {
+    constructor(props) {
+        super(props)
+    }
+    render() {
+        return <div className="cola-btn-wrap"><button></button></div>
+    }
+}
+
+export default Input;
---
+++ src/components/index.js
@@ -0,0 +1,3 @@
+export { default as Alter } from './Alter'
+export { default as Button } from './Button'
+export { default as Shade } from './Shade'
